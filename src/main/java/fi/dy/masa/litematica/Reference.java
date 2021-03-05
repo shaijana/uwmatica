0e489a6e09f7f5bc0ba9dbd654fff7ccf89f09c4
Xannyyy <fabian@meinbuild.de>
2021-03-05 04:11:48 +0100

    renamed

--- src/main/java/fi/dy/masa/litematica/Reference.java
+++ src/main/java/fi/dy/masa/litematica/Reference.java
@@ -4,7 +4,7 @@ import fi.dy.masa.malilib.util.StringUtils;
 
 public class Reference
 {
-    public static final String MOD_ID = "litematica";
-    public static final String MOD_NAME = "Litematica";
+    public static final String MOD_ID = "uwmatica";
+    public static final String MOD_NAME = "UWmatica";
     public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
 }
