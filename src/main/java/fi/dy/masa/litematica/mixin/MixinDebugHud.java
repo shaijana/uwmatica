729ee075169348f0a4c5bdb0fb4b1a20bdfc24f8
Xannyyy <fabian@meinbuild.de>
2021-03-18 03:26:00 +0100

    renamed F3-Menu

--- src/main/java/fi/dy/masa/litematica/mixin/MixinDebugHud.java
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinDebugHud.java
@@ -1,17 +1,18 @@
 package fi.dy.masa.litematica.mixin;
 
-import java.util.List;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.gui.hud.DebugHud;
 import fi.dy.masa.litematica.render.LitematicaRenderer;
 import fi.dy.masa.litematica.render.schematic.WorldRendererSchematic;
 import fi.dy.masa.litematica.world.SchematicWorldHandler;
 import fi.dy.masa.litematica.world.WorldSchematic;
 import fi.dy.masa.malilib.gui.GuiBase;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.DebugHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(DebugHud.class)
 public abstract class MixinDebugHud extends DrawableHelper
@@ -29,10 +30,10 @@ public abstract class MixinDebugHud extends DrawableHelper
 
             WorldRendererSchematic renderer = LitematicaRenderer.getInstance().getWorldRenderer();
 
-            list.add(String.format("%s[Litematica]%s %s C#: %d", pre, rst, renderer.getDebugInfoRenders(), world.getChunkProvider().getLoadedChunks().size()));
+            list.add(String.format("%s[UWmatica]%s %s C#: %d", pre, rst, renderer.getDebugInfoRenders(), world.getChunkProvider().getLoadedChunks().size()));
 
             String str = String.format("E: %d TE: %d", world.getRegularEntityCount(), world.blockEntities.size());
-            list.add(String.format("%s[Litematica]%s %s %s", pre, rst, renderer.getDebugInfoEntities(), str));
+            list.add(String.format("%s[UWmatica]%s %s %s", pre, rst, renderer.getDebugInfoEntities(), str));
         }
     }
 }
